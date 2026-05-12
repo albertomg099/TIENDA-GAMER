c9c35f527e2983a75385d2898db7d27bb5149498
AlbertoMorales <albertomg099@gmail.com>
2026-05-12 13:23:18 -0600

    Se corrige signo de porcentaje en modulo de descuentos

--- descuentos.js
+++ descuentos.js
@@ -4,9 +4,9 @@ export function calcularDescuentos(total) {
         return `Faltan $${faltante} para una promocion.`
     } else if (total >= 200 && total <= 500) {
         let descuento = total * 0.05;
-        return { mensaje: "Tienes un descuento del %5", nuevoTotal: total - descuento }
+        return { mensaje: "Tienes un descuento del 5%", nuevoTotal: total - descuento }
     } else {
         let descuento = total * 0.12;
-        return { mensaje: "Tienes un descuento del %12", nuevoTotal: total - descuento }
+        return { mensaje: "Tienes un descuento del 12%", nuevoTotal: total - descuento }
     }
 }
